df9a8ea20b1d09592a3303b7bc4579856a987705
ZJ <zhangjie@megvii.com>
2019-02-19 18:22:44 +0800

    test

--- shell/test.sh
+++ shell/test.sh
@@ -4,6 +4,11 @@ str="hello world"
 # 0-1:从右边n位起 截取n位字符
 echo ${str:0-1:1}
 
+if [ ! -d "/Users/zj/Documents/repo/git/po-doc/shell" ];
+then
+	echo "not has"
+fi
+
 if [[ ! -n $str || ${str:0-1:1} == d ]]
 then
     echo "Please enter an valid path."
